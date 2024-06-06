825bd16a3c71934725ace7c7c46caa39bf637bca
Daniel-Andres-Rozo-Gomez <daniel.andres.rozo@correounivalle.edu.co>
2024-06-06 01:13:00 -0500

    Se añadio la vista de la terminal y se añadio lo necesario al app pa que el codigo se ejecute

--- Cuarzo_Papiro_Navaja/src/App.java
+++ Cuarzo_Papiro_Navaja/src/App.java
@@ -6,6 +6,13 @@ import Vista.vistaTerminal;
 
 public class App {
     public static void main(String[] args) {
+        modelo modelo = new modelo();
+        vistaGui gui = new vistaGui();
+        vistaTerminal vista = new vistaTerminal();
+        controlador controladorGui = new controlador(modelo, gui);
+        controlador controladorTerminal = new controlador(modelo, vista);
+        controladorGui.mostrarVista();
+        controladorTerminal.mostrarVista();
 
     }
 }
